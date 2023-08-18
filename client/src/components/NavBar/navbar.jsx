922708e6205ca28720abc52b09cf1482104401a3
EloyRocha97 <rochaeloy11@gmail.com>
2023-08-18 14:03:09 -0300

    antes del auth

--- client/src/components/NavBar/navbar.jsx
+++ client/src/components/NavBar/navbar.jsx
@@ -1,12 +1,24 @@
 import { Link } from "react-router-dom";
 import style from "./navbar.module.css";
+import Logo from "../../IMG/G.png";
 
 const Navbar = () => {
   return (
-    <div className={style.nav}>
-      <Link to="/home">Home</Link>
-      <Link to="/detail">Detail</Link>
-    </div>
+    <>
+      <div className={style.nav1}>
+        <Link to="/">
+          <img
+            src={Logo}
+            alt="Logo de GlamourGrove"
+            style={{ width: "45px", height: "20px", marginRight: "10px" }}
+          />
+        </Link>
+      </div>
+      <div className={style.nav}>
+        <Link to="/home">Tienda</Link>
+        <Link to="/detail">Detail</Link>
+      </div>
+    </>
   );
 };
 
--- client/src/views/Landing/landing.jsx
+++ client/src/views/Landing/landing.jsx
@@ -1,8 +1,22 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import Logo from "../../IMG/Glamour.png";
+import style from "./landing.module.css";
+
 const Landing = () => {
   return (
-    <>
-      <h1>Esto es el Landing</h1>
-    </>
+    <div>
+      <Link to="/home">
+        <div style={{ display: "flex", alignItems: "center" }}>
+          <img
+            src={Logo}
+            alt="Logo de GlamourGrove"
+            style={{ width: "200px", height: "50px", marginRight: "10px" }}
+          />
+          {/* <h1 className={style.GlamourGrove}>lamourGrove</h1> */}
+        </div>
+      </Link>
+    </div>
   );
 };
 
