fa79b1a70064320cf9c8fcc5f7d869ed8189185f
EloyRocha97 <rochaeloy11@gmail.com>
2023-10-07 18:44:45 -0300

    landing loading

--- client/src/views/Landing/landing.jsx
+++ client/src/views/Landing/landing.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import Carrusel from "../../components/Carrusel/Carru";
 import { arma1, arma2, gym1, gym2, moda1, moda2 } from "./imgLanding";
@@ -6,6 +6,42 @@ import { arma1, arma2, gym1, gym2, moda1, moda2 } from "./imgLanding";
 import style from "./landing.module.css";
 
 const Landing = () => {
+  const [imagesLoaded, setImagesLoaded] = useState(false);
+
+  useEffect(() => {
+    const images = [arma1, arma2, gym1, gym2, moda1, moda2];
+    let loadedCount = 0;
+
+    const checkAllImagesLoaded = () => {
+      if (loadedCount === images.length) {
+        setImagesLoaded(true);
+      }
+    };
+
+    images.forEach((image) => {
+      const img = new Image();
+      img.src = image;
+      img.onload = () => {
+        loadedCount++;
+        checkAllImagesLoaded();
+      };
+      img.onerror = () => {
+        loadedCount++;
+        checkAllImagesLoaded();
+      };
+    });
+
+    return () => {
+      // Cleanup code if needed
+    };
+  }, []); // Empty dependency array ensures this effect runs once after initial render
+
+  if (!imagesLoaded) {
+    // Mientras se cargan las imágenes, muestra un fondo blanco
+    return <div className={style.loadingContainer}></div>;
+  }
+
+  // Cuando todas las imágenes están cargadas, muestra el contenido de la landing page
   return (
     <div className={style.container}>
       <Carrusel />
