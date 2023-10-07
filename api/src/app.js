73507a3eeeb0f1311d7edb4d0fc30ab6d4cbe5c8
EloyRocha97 <rochaeloy11@gmail.com>
2023-10-07 14:12:39 -0300

    api cors

--- api/src/app.js
+++ api/src/app.js
@@ -1,14 +1,24 @@
 const express = require("express");
 const morgan = require("morgan");
 const cors = require("cors");
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
 const mainRouter = require("./routes");
 
 const app = express();
 
-app.use(morgan("dev"));
 app.use(express.json());
 
-app.use(cors());
+app.use(
+  cors({
+    origin: ["https://e-comerse-back.onrender.com"],
+    credentials: true,
+  })
+);
+app.use(bodyParser.urlencoded({ extended: true, limit: "50mb" }));
+app.use(bodyParser.json({ limit: "50mb" }));
+app.use(cookieParser());
+app.use(morgan("dev"));
 
 app.use(mainRouter);
 
