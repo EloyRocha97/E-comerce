5f9a24abeaf1e982879d6599bb757262ba34644b
EloyRocha97 <rochaeloy11@gmail.com>
2023-10-12 14:38:56 -0300

    basic from

--- client/src/views/index.js
+++ client/src/views/index.js
@@ -2,5 +2,6 @@ import Detail from "./Detail/detail";
 import Home from "./Home/home";
 import Landing from "./Landing/landing";
 import Contacto from "./Contacto/contacto";
+import Form from "./Form/form";
 
-export { Detail, Home, Landing, Contacto };
+export { Detail, Home, Landing, Contacto, Form };
