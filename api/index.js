1e8862ca6e9d7d7f79263e150b802205adb45023
EloyRocha97 <rochaeloy11@gmail.com>
2023-10-16 12:38:13 -0300

    s

--- api/index.js
+++ api/index.js
@@ -1,7 +1,8 @@
 const app = require("./src/app");
 const { sequelize } = require("./src/db");
+const PORT = process.env.PORT || 3001;
 
-app.listen(3001, () => {
+app.listen(PORT, () => {
   sequelize.sync({ force: false });
   console.log("listening on port 3001");
 });
--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -3,7 +3,7 @@ const usersRouter = require("./users.routes");
 const productsRouter = require("./products.routes");
 const authRouter = require("./auth.routes");
 const categoryRouter = require("./category.routes");
-// const tallasRouter = require("./tallas.routes");
+const tallasRouter = require("./tallas.routes");
 
 const mainRouter = Router();
 
@@ -11,6 +11,6 @@ mainRouter.use("/user", usersRouter);
 mainRouter.use("/product", productsRouter);
 mainRouter.use("/", authRouter);
 mainRouter.use("/category", categoryRouter);
-// mainRouter.use("/tallas", tallasRouter);
+mainRouter.use("/tallas", tallasRouter);
 
 module.exports = mainRouter;
